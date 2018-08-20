4aa79de4cdd5d77996cca25f36f7d85e5e8e8f48
Merry Nguyen <merryannnguyen@yahoo.com>
2018-08-20 14:21:42 -0600

    added some scripts for practice

---
+++ 2670Fall/Assets/EndGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+	public FloatData Health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health.Value < 0)
+		{
+			//End Game
+		}
+	}
+}
---
+++ 2670Fall/Assets/IfStatements.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	public float Speed;
+	public bool LightsOn;
+	
+	// Use this for initialization
+	void Start () {
+		if (LightsOn)
+		{
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
