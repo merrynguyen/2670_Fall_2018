e16b01cf46ed61a6dfdc454146344fdbae2d61da
Unknown <merryannnguyen@yahoo.com>
2018-09-05 14:15:46 -0600

    working on character movement

---
+++ 2670Fall/Assets/Scripts/FloatData.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat", menuName = "AR/FloatData")]
+public class FloatData : ScriptableObject
+{
+	public float value;
+	
+	public float Value
+	{
+		get { return value;}
+
+	}
+}
+	
---
+++ 2670Fall/Assets/Scripts/FloatInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName= "FloatInput")]
+public class FloatInput : FloatData
+{
+    public string InputType;
+    
+    void UpdateInput()
+    {
+        
+    }
+}
